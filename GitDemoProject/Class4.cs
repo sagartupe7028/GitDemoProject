35f6ec158e6fe3fbf3ce86d0ebfdd26d8ee24d14
Sagar Tupe <sagarT@digitalinfobytes.com>
2024-06-03 15:03:47 +0530

    Local Changes Class4

---
+++ GitDemoProject/Class4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoProject
+{
+    class Class4
+    {
+        public int MyProperty { get; set; }
+        public int a { get; set; }
+        public int v { get; set; }
+        public int c { get; set; }
+    }
+}
