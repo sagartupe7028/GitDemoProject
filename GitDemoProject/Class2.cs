cea1e729cdc45e85c7ed03c33bf4260e6099544d
Sagar Tupe <sagarT@digitalinfobytes.com>
2024-06-03 15:01:14 +0530

    Chahes Made on master

---
+++ GitDemoProject/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoProject
+{
+    class Class2
+    {
+        public int MyProperty { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ GitDemoProject/Class3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoProject
+{
+    class Class3
+    {
+        public int MyProperty { get; set; }
+        public int phone { get; set; }
+    }
+}
