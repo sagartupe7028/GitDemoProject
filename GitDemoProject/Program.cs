d91634e6524d547d2fd65d59565affbe16dab786
Sagar Tupe <sagarT@digitalinfobytes.com>
2024-06-03 12:43:42 +0530

    New Message  Added

--- GitDemoProject/Program.cs
+++ GitDemoProject/Program.cs
@@ -10,7 +10,7 @@ namespace GitDemoProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello word");
         }
     }
 }
